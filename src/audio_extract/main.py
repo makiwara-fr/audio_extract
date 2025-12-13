be1298e6690ee01db87864b271db9f4e8a1571c8
makiwara-fr <charles@makiwara.fr>
2025-12-13 16:19:00 +0100

    minor fix

--- src/audio_extract/main.py
+++ src/audio_extract/main.py
@@ -19,218 +19,75 @@
 # ######################################################################
 
 
-import sys
-import re
-import os
+
+import argparse
 from pathlib import Path
-import yaml
-from yaml.loader import SafeLoader
 
 
-def set_regexp(suffix_list):
-    """set the regex to look for audio files"""
-    regexp = ""
-    for suf in suffix_list:
-        if regexp == "":
-            regexp += f"({suf}"
-        else:
-            regexp += f"|{suf}"
-    # finalize the string if not empty
-    if regexp != "":
-        regexp += f")"
-        return re.compile(regexp)
-    else:
-        return None
+
+from audio_extract.process import process
 
 
-def scan_folder(wd, regexp):
-    """scan folder for files mathcing regexp"""
-    files_list = []
-
-    # Scanning folder
-    print(f"Scanning folder : {wd}")
-
-    if wd == ".":
-        wd = os.getcwd()
-
-    # scanning all the files  #for entry in os.scandir(wd):
-    for entry in Path(wd).iterdir():
-        if entry.is_file():
-            # recognize if audio
-            if regexp.search(entry.name):
-                # print(entry.name)
-                files_list.append(entry)
-        else:
-            # don't go into subdirectories
-            pass
-
-    return files_list
-
-
-def extract(file, output_dir, beg, end, ffmpeg_path):
-    """extract except from given file and put results in output dir taking audio only for beg to end"""
-    infile = Path.cwd().joinpath(file)
-    print(f"Processing {infile}")
-    print()
-    outfile = Path.cwd().joinpath(output_dir, file.stem + "_extract.mp3")
-
-    try:
-        # reencode if not MP3 file
-        if infile.suffix != ".mp3":
-            print("not mp3", infile.suffix)
-            reencode = " -codec:a libmp3lame "
-        else:
-            reencode = ""
-
-        # command = f"ffmpeg -i \"{infile}\" -ss {beg} -to {end}  -af \"afade=t=out:st={end-5}:d=5\" {reencode} -y \"{outfile}\""
-        command = f'{ffmpeg_path} -i "{infile}" -ss {beg} -to {end}  -af "afade=t=out:st={end-5}:d=5" {reencode} -y "{outfile}"'
-
-        status = os.system(command)
-        return status
-
-    except Exception as e:
-        print(e)
-        return 1
-
-
-def process(input_params=None):
-    # Welcome messages
-    print("")
-    print("")
-    print("")
-    print(
-        "".join(
-            (
-                "                 __   .__                              \n",
-                "   _____ _____  |  | _|__|_  _  _______ ____________   \n",
-                "  /     \\\\__  \ |  |/ /  \ \/ \/ /\__  \\\\_  __ \\__  \\  \n",
-                " |  Y Y  \/ __ \|    <|  |\     /  / __ \|  | \// __ \_\n",
-                " |__|_|  (____  /__|_ \__| \/\_/  (____  /__|  (____  /\n",
-                "      \/     \/     \/                \/           \/ \n",
-            )
-        )
+def main():
+    parser = argparse.ArgumentParser(
+        description="Extracts audio excerpts from audio files."
     )
-    print("")
-    print("")
-    print("")
-    print("-------------")
-    print("Audio extract")
-    print("-------------")
+    parser.add_argument(
+        "-d", "--debug", action="store_true", help="Enable debug mode."
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        help="Input directory containing audio files.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Output directory for extracted audio files.",
+    )
+    parser.add_argument(
+        "-s",
+        "--start",
+        type=int,
+        help="Starting second for the audio excerpt.",
+    )
+    parser.add_argument(
+        "-e",
+        "--end",
+        type=int,
+        help="Ending second for the audio excerpt.",
+    )
+    parser.add_argument(
+        "-f",
+        "--fade",
+        type=int,
+        help="Fade duration in seconds (0 for no fade).",
+    )
 
-    # print(input_params)
+    args = parser.parse_args()
 
-    if input_params != None:  # input parameters are given probably from Gui
-        params = {}
-        for k, v in input_params.items():
-            params[k] = v
+    input_params = {}
+    if args.debug:
+        input_params["debug"] = True
+    if args.input:
+        input_params["input_dir"] = Path(args.input)
+    if args.output:
+        input_params["output_dir"] = Path(args.output)
+    if args.start is not None:
+        input_params["first_second"] = args.start
+    if args.end is not None:
+        input_params["last_second"] = args.end
+    if args.fade is not None:
+        input_params["fade_d"] = args.fade
 
-    else:  # Open the config file and load the file
-        try:
-            with open("parameters.yaml") as f:
-                params = yaml.load(f, Loader=SafeLoader)
-        except:
-            # not file found
-            params = {}
-
-    # print(params)
-
-    # list of default parameters
-    default_params = {
-        "input_dir": ".",
-        "output_dir": "output",
-        "first_second": 0,
-        "last_second": 60,
-        "input_file_extension": ["wav", "mp3"],
-        "path_ffmpeg": "",
-        "debug": True,
-        "fade_d": 8,
-    }
-
-    # check quality of parameters
-    for k, v in default_params.items():
-        if k not in params:
-            print(f"{k} is defaulted")
-            params[k] = default_params[k]
-
-    debug = params["debug"]
-
-    print(params)
-
-    # read input files
-    # ----------------
-    if debug:
-        print(f"Working directory is {Path.cwd()}")
-
-    regex = set_regexp(
-        params["input_file_extension"]
-    )  # list of extension to look for in directory
-
-    files_list = scan_folder(params["input_dir"], regex)  # scanning the input directory
-    if debug:
-        if len(files_list) > 0:
-            print(f"found {len(files_list)} files")
-            print()
-        else:
-            print("exiting no file has been found")
-            print("")
-            sys.exit(0)
-
-    end = params["last_second"]
-    beg = params["first_second"]
-    if not isinstance(params["output_dir"], Path):
-        output_dir = params["output_dir"] + "/"
+    if input_params:
+        process(input_params=input_params)
     else:
-        output_dir = params["output_dir"]
-    # --------------------------
-    # launch ffmpeg on file list
-    # --------------------------
-    status = 0
-    error_files = []
+        process()
 
-    # check if output directory exists. Otherwise create it
-    # -----------------------------------------------------
-    if not Path(params["output_dir"]).exists():
-        print("Output directory doesn't exist. Creating it")
-        Path(params["output_dir"]).mkdir(parents=True, exist_ok=True)
-
-    # path to FFMPEG
-    # --------------
-    if len(params["path_ffmpeg"]) > 0:
-        ffmpeg_path = Path(params["path"]).joinpath("ffmpeg")
-    else:
-        ffmpeg_path = "ffmpeg"
-
-    if debug:
-        print(f"Path to ffmpeg is : {ffmpeg_path}")
-        print()
-
-    # real processing of files
-    # -------------------------
-
-    for f in files_list:
-        if extract(f, output_dir, beg, end, ffmpeg_path) == 0:
-            status += 1
-        else:
-            error_files.append(f)
-
-    # --------------
-    # report results
-    # --------------
-
-    # check if all files have been processed
-    if len(files_list) > 0:
-        results = status / len(files_list)
-    else:
-        results = 1.0
-    print(f"Processing done at {results*100:.0f}%")
-    print()
-
-    if len(error_files) > 0:
-        print("Something went wrong with these files")
-        for ef in error_files:
-            print("-", ef.name)
-    print("")
 
 
 if __name__ == "__main__":
-    process()
+    main()
